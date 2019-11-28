d741006b47efb6d344471deed0a26838cbcbe00c
SunnyGJing <806416908@qq.com>
2019-11-28 13:27:08 -0600

    feat:Implement add/delete/show favorite tutorial

--- src/app/tutorials/tutorials.page.ts
+++ src/app/tutorials/tutorials.page.ts
@@ -1,22 +1,30 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { AlertController, IonList, LoadingController, ModalController, ToastController } from '@ionic/angular';
-import { UserData } from '../providers/user-data';
-import { TutorialData } from '../providers/tutorial-data';
-import { Router } from '@angular/router';
-import { TutorialFilterPage } from '../pages/tutorial-filter/tutorial-filter.page';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import {
+  AlertController,
+  IonList,
+  LoadingController,
+  ModalController,
+  ToastController
+} from "@ionic/angular";
+import { UserData } from "../providers/user-data";
+import { TutorialData } from "../providers/tutorial-data";
+import { Router } from "@angular/router";
+import { TutorialFilterPage } from "../pages/tutorial-filter/tutorial-filter.page";
+import { InAppBrowser } from "@ionic-native/in-app-browser/ngx";
 
 @Component({
-  selector: 'app-tutorials',
-  templateUrl: './tutorials.page.html',
-  styleUrls: ['./tutorials.page.scss'],
+  selector: "app-tutorials",
+  templateUrl: "./tutorials.page.html",
+  styleUrls: ["./tutorials.page.scss"],
+  providers: [InAppBrowser]
 })
 export class TutorialsPage implements OnInit {
   // @ViewChild('scheduleList', { static: true }) scheduleList: IonList;
-  @ViewChild('tutorialList', {}) tutorialList: IonList;
+  @ViewChild("tutorialList", {}) tutorialList: IonList;
 
   category = 0;
-  queryText = '';
-  segment = 'all';
+  queryText = "";
+  segment = "all";
   excludeTracks: any = [];
   shownTutorials: any = [];
   groups: any = [];
@@ -29,8 +37,15 @@ export class TutorialsPage implements OnInit {
     public modalCtrl: ModalController,
     public router: Router,
     public toastCtrl: ToastController,
-    public user: UserData
-  ) { }
+    public user: UserData,
+    public iab: InAppBrowser
+  ) {}
+
+  //route function to app browser
+  tutorialClick(tracks) {
+    const browser = this.iab.create(tracks, "_blank", "toolbar=no,location=no");
+    browser.show();
+  }
 
   ngOnInit() {
     this.updateTutorial();
@@ -40,11 +55,17 @@ export class TutorialsPage implements OnInit {
     if (this.tutorialList) {
       this.tutorialList.closeSlidingItems();
     }
-    this.tutorialData.getTCategory(this.category, this.queryText, this.excludeTracks, this.segment)
-    .subscribe((data:any) => {
-      this.shownTutorials = data.shownTutorials
-      this.groups = data.groups
-    })
+    this.tutorialData
+      .getTCategory(
+        this.category,
+        this.queryText,
+        this.excludeTracks,
+        this.segment
+      )
+      .subscribe((data: any) => {
+        this.shownTutorials = data.shownTutorials;
+        this.groups = data.groups;
+      });
   }
 
   async presentFilter() {
@@ -65,34 +86,40 @@ export class TutorialsPage implements OnInit {
     if (this.user.hasFavorite(sessionData.name)) {
       // woops, they already favorited it! What shall we do!?
       // prompt them to remove it
-      this.removeFavorite(slidingItem, sessionData, 'Favorite already added');
+      this.removeFavorite(slidingItem, sessionData, "Favorite already added");
     } else {
       // remember this session as a user favorite
       this.user.addFavorite(sessionData.name);
 
       // create an alert instance
       const alert = await this.alertCtrl.create({
-        header: 'Favorite Added',
-        buttons: [{
-          text: 'OK',
-          handler: () => {
-            // close the sliding item
-            slidingItem.close();
+        header: "Favorite Added",
+        buttons: [
+          {
+            text: "OK",
+            handler: () => {
+              // close the sliding item
+              slidingItem.close();
+            }
           }
-        }]
+        ]
       });
       // now present the alert on top of all other content
       await alert.present();
     }
   }
 
-  async removeFavorite(slidingItem: HTMLIonItemSlidingElement, sessionData: any, title: string) {
+  async removeFavorite(
+    slidingItem: HTMLIonItemSlidingElement,
+    sessionData: any,
+    title: string
+  ) {
     const alert = await this.alertCtrl.create({
       header: title,
-      message: 'Would you like to remove this session from your favorites?',
+      message: "Would you like to remove this session from your favorites?",
       buttons: [
         {
-          text: 'Cancel',
+          text: "Cancel",
           handler: () => {
             // they clicked the cancel button, do not remove the session
             // close the sliding item and hide the option buttons
@@ -100,7 +127,7 @@ export class TutorialsPage implements OnInit {
           }
         },
         {
-          text: 'Remove',
+          text: "Remove",
           handler: () => {
             // they want to remove this session from their favorites
             this.user.removeFavorite(sessionData.name);
@@ -115,6 +142,4 @@ export class TutorialsPage implements OnInit {
     // now present the alert on top of all other content
     await alert.present();
   }
-
-
 }
